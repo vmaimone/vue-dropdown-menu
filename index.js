a367d23a0356eab8f0beb909d568b1f1b151ba67
vmaimone <vmaimone@users.noreply.github.com>
2016-12-16 10:42:21 -0500

    add installation method

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('./dropdown-menu.vue')
+var dropdown = require('./dropdown-menu.vue')
+dropdown.install = function(Vue, options) {
+  options = options || { name: 'dropdown' }
+  Vue.component(options.name, dropdown)
+}
+module.exports = dropdown
